f9d49ca868a87d4eec9bbf68f7cf3befdab753fe
madddanz <maddiedanzberger@gmail.com>
2018-07-31 20:14:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,21 +2,44 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-get '/reversename/:name'
+get '/reversename/:name' do
   @user_name = params[:name]
   "#{@user_name.reverse}"
 end
 
-# get '/square/:number'
-#   num1 = params [:number].to_i
-#   (num1*num1).to_s
-# end 
+get '/square/:number' do
+  num1 = params[:number]
+  (num1.to_i**2).to_s
+end 
 
-# get '/say/:number/:phrase'
-#   @phrase = params[:phrase]
-#   @num1 = params[:number]
-#   "#{@phrase}*#{@num1}"
-# end 
+get '/say/:number/:phrase' do
+num1 = params[:number].to_i 
+@phrase = params[:phrase]
+(@phrase * num1)
+end 
 
-# get '/say/:word1/:word2/:word3/:word4/:word5'
-  
+get '/say/:word1/:word2/:word3/:word4/:word5' do
+  phrase = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+end
+
+get '/add/:num1/:num2' do
+  num1 = params[:num1].to_i 
+  num2 = params[:num2].to_i 
+  (num1 + num2).to_s 
+end 
+get '/subtract/:num1/:num2' do
+  num1 = params[:num1].to_i 
+  num2 = params[:num2].to_i 
+  (num1 - num2).to_s 
+end 
+get '/multiply/:num1/:num2' do
+  num1 = params[:num1].to_i 
+  num2 = params[:num2].to_i 
+  (num1 * num2).to_s 
+end 
+get '/divide/:num1/:num2' do
+  num1 = params[:num1].to_i 
+  num2 = params[:num2].to_i 
+  (num1 / num2).to_s 
+end 
+end
